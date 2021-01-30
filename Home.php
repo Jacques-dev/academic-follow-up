b1607bab4273b37898303842a1a676e1588de608
Jacques <jacques.tellier@hotmail.com>
2021-01-30 12:24:53 +0100

    Navbar

--- Home.php
+++ Home.php
@@ -62,6 +62,29 @@
       </script>
     <?php } ?>
 
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="Home.php">MAFU</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="Home.php">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Vue/Averages.php">Mes moyennes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Vue/Ranking.php">Mon classement</a>
+          </li>
+          <!-- <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li> -->
+        </ul>
+      </div>
+    </nav>
+
     <div class="container">
       <div class="row">
         <div class="col-lg-12" align="center" id="head-column">
