1093ee7578c5a8bd35c4fae174defb85ca2334f9
Jacques <jacques.tellier@hotmail.com>
2021-01-30 11:50:41 +0100

    Register & Login fait

--- Home.php
+++ Home.php
@@ -63,7 +63,16 @@
     <?php } ?>
 
     <div class="container">
-
+      <div class="row">
+        <div class="col-lg-12" align="center" id="head-column">
+          My Academic Follow-Up<br>
+          <?php
+            if (isset($_SESSION["email"])):
+              echo $_SESSION["email"];
+            endif;
+          ?>
+        </div>
+      </div>
     </div>
 
     <script src="Controller/js/app.js"></script>
